aab66be96d729678dc29e41526066dc0010277b4
Aakash Shrestha <Aakash.Shrestha@ui.city>
2022-08-25 11:24:07 +1000

    saving first file

--- test.py
+++ test.py
@@ -1,13 +1,18 @@
 import requests
+import json
 
 url = "https://api.tomorrow.io/v4/timelines"
 
 querystring = {
-"location":"-26.6559759, -153.0918365",
+"location":"-26.6559759, -153.0918365",         #lat long of sunshine coast 
 "fields":["temperature"],
-"units":"metric",
-"timesteps":"1d",
+"units":"metric",                               #celcius    
+"timesteps":"1d",                               #one day for a month
 "apikey":"VM5OHj1SWudXbY1U1OWELCO4Iyp5q6xR"}
 
 response = requests.request("GET", url, params=querystring)
 print(response.text)
+
+data = json.loads(response.text)
+with open('weather.json','w') as f:   #loggin the data into json file
+    json.dump(data, f, indent=2)
