897002cff2973f3edb1dfc12daac5fbc16695d7d
Aakash Shrestha <Aakash.Shrestha@ui.city>
2022-10-13 20:53:54 +1000

    managing directory

---
+++ test files/database.py
@@ -0,0 +1,67 @@
+<<<<<<< HEAD
+from influxdb_client import InfluxDBClient
+from influxdb_client.client.write_api import SYNCHRONOUS
+from datetime import datetime
+import json
+
+
+# returns JSON object as
+# a dictionary
+
+app_version = 1.30
+with open('weather.json', 'r') as file:
+    json_data = json.load(file)
+
+token = "whcBdFBOMnUYN4cg7awyH-k8xGNmpxFgiFop6eX4OT68RTgcmd-Vt-B5L3XgNf2S58-I93DH7pFeoKPYCqZjCQ=="
+org = "Aakash.shrestha@ui.city"
+bucket = "weather"
+
+with InfluxDBClient(url="http://localhost:8086", token=token, org=org, default_tags={'appversion': str(app_version)}, debug=False) as client:
+    def flatten_interval(interval):
+        values = interval['values']
+        values['startTime'] = interval['startTime']
+        return values
+
+
+    # get timelines
+    timelines = json_data['data']['timelines']
+    # get intervals
+    intervals = list(map(lambda timeline: timeline['intervals'], timelines))
+    # flatten intervals
+    intervals = [flatten_interval(interval) for sublist in intervals for interval in sublist]
+    
+    # write data
+    client.write_api(write_options=SYNCHRONOUS).write(bucket=bucket,
+                                                      record=intervals,
+                                                      record_measurement_name="weather",
+                                                      record_time_key="startTime",
+                                                      record_field_keys=["humidity", "temperature", "windSpeed"])
+=======
+import json
+import influxdb_client, os, time
+from influxdb_client import InfluxDBClient, Point, WritePrecision
+from influxdb_client.client.write_api import SYNCHRONOUS
+
+token = os.environ.get("INFLUXDB_TOKEN")
+org = "aakash.shrestha@ui.city"
+url = "https://ap-southeast-2-1.aws.cloud2.influxdata.com"
+
+client = influxdb_client.InfluxDBClient(url=url, token=token, org=org)
+
+bucket="weather"
+
+with open('weather.json', 'r') as file:
+    json_data = json.load(file)
+
+write_api = client.write_api(write_options=SYNCHRONOUS)
+   
+for value in range(5):
+  point = (
+    Point("measurement")
+    .tag("tagname1", "tagvalue1")
+    .field(json_data, value)
+  )
+  write_api.write(bucket=bucket, org="aakash.shrestha@ui.city", record=point)
+  time.sleep(1) # separate points by 1 second
+
+>>>>>>> b99c2b0 (adding database)
---
+++ test files/main.py
@@ -0,0 +1,39 @@
+import requests
+import json
+
+url = "https://api.tomorrow.io/v4/timelines"
+
+querystring = {
+"location":"-26.6559759, 153.0918365",         #lat long of sunshine coast 
+"fields":["temperature","sunriseTime","sunsetTime","windSpeed","humidity"],
+"units":"metric",                               #celcius    
+"timesteps":"1h",  
+"startTime":"now",
+"endTime":"nowPlus1h",                             
+"apikey":"VM5OHj1SWudXbY1U1OWELCO4Iyp5q6xR"}
+
+def switch():
+    while True:
+        command =  input("Do you want the current weather data? :: ").lower().strip()
+        if command == "yes":
+            start()
+            pass
+        elif command == "no":
+            continue
+        elif command == "quit":
+            break
+        else:
+            print("wrong command")
+            continue
+
+def start():
+    response = requests.request("GET", url, params=querystring)
+    print(response.text)
+
+    data = json.loads(response.text)
+    with open('weather.json','w') as f:   #loggin the data into json file
+        json.dump(data, f, indent=2)
+
+
+if __name__=="__main__":
+    switch()
