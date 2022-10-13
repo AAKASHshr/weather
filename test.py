3c1fd150340a39e98bb5fc4271be64fd88488d38
AAKASHshr <Aakash.Shrestha@ui.city>
2022-10-13 14:10:03 +1000

    changes to test file

--- test.py
+++ test.py
@@ -10,13 +10,10 @@ querystring = {
 "timesteps":"current",                               
 "apikey":"VM5OHj1SWudXbY1U1OWELCO4Iyp5q6xR"}
 
-def start():
-    response = requests.request("GET", url, params=querystring)
-    print(response.text)
+
+response = requests.request("GET", url, params=querystring)
+print(response.text)
 
 data = json.loads(response.text)
 with open('weather.json','w') as f:   #loggin the data into json file
     json.dump(data, f, indent=2)
-broker = "ws://localhost:8008"
-post = requests.post(broker, json = data)
-print(post.text)
